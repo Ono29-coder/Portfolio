d5ef94af5c84b8ad6a82bfe8e39cdbf485622941
Oh!No! <onoyukie.24@gmail.com>
2025-01-02 16:36:46 +0900

    Delete opus.js

--- opus.js
+++
@@ -1,9 +0,0 @@
-function showImage(src) {
-    const preview = document.getElementById("preview");
-    preview.src = src; // src属性を更新して画像を変更
-}
-
-// ページ読み込み時に最初の画像を表示
-window.onload = function () {
-    showImage('./img/game-3.gif');
-};
